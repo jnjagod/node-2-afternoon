c7ecdb81947e95148f5ae545e8c6d83086fd873b
jnjagod <josh.jagoda@gmail.com>
2019-10-18 13:25:06 -0700

    various edits

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ const app =  express()
 app.use(express.json())
 app.use(express.static(__dirname + '/../public/build'))
 
-const baseUrl = '/api/messages'
-app.post(baseUrl, mc.create)
-app.get(baseUrl, mc.read)
-app.put(`${baseUrl}/:id`, mc.update)
-app.delete(`${baseUrl}/:id`, mc.delete)
+const messagesUrl = '/api/messages'
+app.post(messagesUrl, mc.create)
+app.get(messagesUrl, mc.read)
+app.put(`${messagesUrl}/:id`, mc.update)
+app.delete(`${messagesUrl}/:id`, mc.delete)
 
 const port = 3001
 app.listen(port, () => console.log('listening on port', port))
